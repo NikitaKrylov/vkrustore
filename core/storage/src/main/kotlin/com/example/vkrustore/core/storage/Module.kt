6b0c3422befb7aac6b92952629a9f3fb2da6ffa2
Andrey Kirilin <andreimipad43@mail.ru>
2026-02-17 11:23:15 +0300

    add datastore

--- core/storage/src/main/kotlin/com/example/vkrustore/core/storage/Module.kt
+++ core/storage/src/main/kotlin/com/example/vkrustore/core/storage/Module.kt
@@ -1,7 +1,29 @@
 package com.example.vkrustore.core.storage
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import kotlinx.serialization.json.Json
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val storageModule = module {
+    single<DataStore<Preferences>> {
+        PreferenceDataStoreFactory.create(
+            produceFile = {
+                androidContext().preferencesDataStoreFile("app_prefs")
+            }
+        )
+    }
 
+    single<Json> {
+        Json {
+            ignoreUnknownKeys = true
+        }
+    }
+
+    single<KeyValueStorage> {
+        KeyValueStorageImpl(get(), get())
+    }
 }
