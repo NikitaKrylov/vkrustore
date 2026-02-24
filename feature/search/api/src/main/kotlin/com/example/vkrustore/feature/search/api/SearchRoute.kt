6dce9eb62aa80ace0f208e5af58ebb456e7d00a7
nikitakrylov <krylovne@mts.ru>
2026-02-24 22:27:01 +0300

    add nav bottom bar

--- feature/search/api/src/main/kotlin/com/example/vkrustore/feature/search/api/SearchRoute.kt
+++ feature/search/api/src/main/kotlin/com/example/vkrustore/feature/search/api/SearchRoute.kt
@@ -1,6 +1,7 @@
 package com.example.vkrustore.feature.search.api
 
+import com.example.vkrustore.core.navigation.BaseRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object SearchRoute
+data object SearchRoute : BaseRoute
